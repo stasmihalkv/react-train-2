30bf31ff0c1dbb3fa78c0187cb4459b81562a296
stasmihalkv <stasmihalkv@gmail.com>
2020-06-16 23:02:45 +0300

    Updates

---
+++ precache-manifest.e5d46453ab083365f50d99303b34ad5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1049bd52ffeec519966abdf5abf668",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "d9297d397b25aa040d37",
+    "url": "/hello/static/css/main.a814a865.chunk.css"
+  },
+  {
+    "revision": "7973c840818fec9928c0",
+    "url": "/hello/static/js/2.3440da90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello/static/js/2.3440da90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9297d397b25aa040d37",
+    "url": "/hello/static/js/main.b5df875d.chunk.js"
+  },
+  {
+    "revision": "b4d0f9b21d17ca20aeb5",
+    "url": "/hello/static/js/runtime-main.e120f1c1.js"
+  }
+]);
